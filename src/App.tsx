d3a8a303a673fd513dfe6c73db0ea1fc444b4f82
makrenka <smokrenko79@gmail.com>
2023-04-19 19:10:33 +0300

    add styles to the CharList

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { QueryClient, QueryClientProvider } from 'react-query';
 
-import { CharList } from './components/CharList/CharList';
+import { CharList } from './components/CharList';
 
 import './App.css';
 
--- src/components/CharList/CharList.tsx
+++ src/components/CharList/CharList.tsx
@@ -1,21 +1,30 @@
-import { useCharacter } from "../../hooks/useCharacter"
+import { useCharacter } from "../../hooks/useCharacter";
+import { Spinner } from "../Spinner";
+
+import './CharList.scss';
 
 export const CharList = () => {
 
     const { characters, error, fetchNextPage, hasNextPage, status } = useCharacter();
+    console.log(characters?.info.next.split('='))
+    if (status === 'loading') return <Spinner />;
+
+    if (status === 'error') return <p>Ups!, {`${error}`}</p>;
 
     return (
-        <ul className="char-list__grid">
-            {characters && characters.results.map(character => (
-                <li
-                    className="char-list__grid-item"
-                    // onClick={() => { onModal(character.id) }}
-                    key={character.id}
-                >
-                    <img src={character.image} alt={character.name} className='char-list__grid-item-img' />
-                    <p className='char-list__grid-item-title'>{character.name}</p>
-                </li>
-            ))}
-        </ul>
+        <div className="char-list">
+            <ul className="char-list__grid">
+                {characters && characters.results.map(({ id, image, name }) => (
+                    <li
+                        className="char-list__grid-item"
+                        // onClick={() => { onModal(character.id) }}
+                        key={id}
+                    >
+                        <img src={image} alt={name} className='char-list__grid-item-img' />
+                        <p className='char-list__grid-item-title'>{name}</p>
+                    </li>
+                ))}
+            </ul>
+        </div>
     )
 }
