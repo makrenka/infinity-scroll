328417f69a20b20832bc79f5c819a1aca9d3f917
makrenka <smokrenko79@gmail.com>
2023-04-19 19:51:21 +0300

    add InfiniteScroll

--- src/components/CharList/CharList.tsx
+++ src/components/CharList/CharList.tsx
@@ -1,3 +1,4 @@
+import InfiniteScroll from "react-infinite-scroll-component";
 import { useCharacter } from "../../hooks/useCharacter";
 import { Spinner } from "../Spinner";
 
@@ -6,25 +7,32 @@ import './CharList.scss';
 export const CharList = () => {
 
     const { characters, error, fetchNextPage, hasNextPage, status } = useCharacter();
-    console.log(characters?.info.next.split('='))
+
     if (status === 'loading') return <Spinner />;
 
-    if (status === 'error') return <p>Ups!, {`${error}`}</p>;
+    if (status === 'error') return <p>Ups!, {`${error}` as string}</p>;
 
     return (
         <div className="char-list">
-            <ul className="char-list__grid">
-                {characters && characters.results.map(({ id, image, name }) => (
-                    <li
-                        className="char-list__grid-item"
-                        // onClick={() => { onModal(character.id) }}
-                        key={id}
-                    >
-                        <img src={image} alt={name} className='char-list__grid-item-img' />
-                        <p className='char-list__grid-item-title'>{name}</p>
-                    </li>
-                ))}
-            </ul>
+            <InfiniteScroll
+                dataLength={characters ? characters.results.length : 0}
+                next={() => fetchNextPage()}
+                hasMore={!!hasNextPage}
+                loader={<Spinner />}
+            >
+                <ul className="char-list__grid">
+                    {characters && characters.results.map(({ id, image, name }) => (
+                        <li
+                            className="char-list__grid-item"
+                            // onClick={() => { onModal(character.id) }}
+                            key={id}
+                        >
+                            <img src={image} alt={name} className='char-list__grid-item-img' />
+                            <p className='char-list__grid-item-title'>{name}</p>
+                        </li>
+                    ))}
+                </ul>
+            </InfiniteScroll>
         </div>
     )
 }
